0aa6d4966d19db894914cadf5b00e275c815a9e3
f-chilmi <68938896+f-chilmi@users.noreply.github.com>
2021-03-24 00:08:42 +0700

    read data, searching and linking

--- App.js
+++ App.js
@@ -5,35 +5,101 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
+  FlatList,
+  TextInput,
+  ActivityIndicator,
+  Linking
 } from 'react-native';
 
-const MyGithub = () => {
+const App = () => {
+  const [data, setData] = React.useState(null);
+  const [newData, setNewData] = React.useState(data);
+  const [text, onChangeText] = React.useState('');
+
+  const url = 'https://api.github.com/users/f-chilmi/repos';
+  React.useEffect(() => {
+    fetch(url)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json();
+        }
+      })
+      .then(resJson => {
+        setData(resJson);
+        setNewData(resJson);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    if (data !== null) {
+      const found = data.filter(item => item.name.includes(text));
+      text === '' ? setNewData(data) : setNewData(found);
+    }
+  }, [text]);
+
+  console.log('data', data);
+
+  const renderItem = ({item}) => (
+    <TouchableOpacity style={styles.item} onPress={() => handleClick(item.html_url)}>
+      <Text style={styles.title}>{item.name}</Text>
+    </TouchableOpacity>
+  );
+
+  if (data === null) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  const handleClick = uri => {
+    Linking.canOpenURL(uri).then(supported => {
+      if (supported) {
+        Linking.openURL(uri);
+      } else {
+        console.log("Don't know how to open URI: " + uri);
+      }
+    });
+  };
+
   return (
-    <View>
-      <Text>My Github</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Search repository"
+      />
+      <FlatList
+        data={newData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    paddingHorizontal: 15,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  item: {
+    backgroundColor: '#f9c4ba',
+    padding: 10,
+    borderRadius: 10,
+    margin: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontSize: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  input: {
+    borderRadius: 50,
+    paddingHorizontal: 15,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
 
-export default MyGithub;
+export default App;
