f2b5a21e0e6a10c72f6db118b098e414f0c5f6e3
f-chilmi <68938896+f-chilmi@users.noreply.github.com>
2021-03-24 11:02:01 +0700

    add some texts and tidying code

--- App.js
+++ App.js
@@ -47,14 +47,26 @@ const App = () => {
   const FirstPage = () => {
     return (
       <View style={styles.container}>
-        <TextInput
-          onChangeText={setUsername}
-          style={styles.inputUsername}
-          value={username}
-          placeholder={'Input username github here'}
-          autoFocus={true}
-        />
-        <Button title="Submit" onPress={fetchData} style={styles.button} />
+        <View style={styles.wrapper}>
+          <Text style={styles.title0}>Welcome!</Text>
+          <Text style={styles.subtitle}>
+            Input github username for getting repository list
+          </Text>
+          <TextInput
+            onChangeText={setUsername}
+            style={styles.inputUsername}
+            value={username}
+            placeholder={'Input username github here'}
+            autoFocus={true}
+          />
+          <Button
+            title="Submit"
+            onPress={fetchData}
+            color="#e0a39c"
+            accessibilityLabel="Learn more about this purple button"
+            style={styles.button}
+          />
+        </View>
       </View>
     );
   };
@@ -75,14 +87,13 @@ const App = () => {
     Linking.canOpenURL(uri).then(supported => {
       if (supported) {
         Linking.openURL(uri);
-      } else {
-        console.log("Don't know how to open URI: " + uri);
       }
     });
   };
 
   return (
     <SafeAreaView style={styles.container}>
+      <Text style={styles.uname}> {username} List Repository </Text>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
@@ -101,8 +112,23 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 15,
+    paddingHorizontal: 10,
     marginTop: StatusBar.currentHeight || 0,
+  },
+  uname: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  wrapper: {
+    marginTop: '15%',
+  },
+  title0: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subtitle: {
+    marginBottom: 10,
   },
   item: {
     backgroundColor: '#f9c4ba',
@@ -124,8 +150,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     paddingHorizontal: 15,
     height: 40,
-    marginTop: '50%',
-    marginBottom: '5%',
+    marginBottom: 20,
     borderWidth: 1,
   },
   button: {
